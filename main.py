33c940aaa99c811889593779c01b931fb965a429
Fabiano Marques <111817663+MarquesFabiano@users.noreply.github.com>
2023-09-14 20:38:13 -0300

    update

--- main.py
+++ main.py
@@ -16,20 +16,28 @@ def BuscaFilme(nome_filme, api_key):
 
         if dados["total_results"] > 0:
             resultado = dados["results"][0]
-
-            nome = resultado["title"]
-            descricao = resultado["overview"]
-            ano = resultado["release_date"].split("-")[0]
-
-            print(f"Nome do Filme: {nome}")
-            print(f"Ano: {ano}")
-            print(f"Descrição: {descricao}")
+            return resultado
         else:
-            print("Nenhum resultado encontrado para este filme. Revise o texto e digite novamente!")
+            print("Nenhum resultado encontrado para este filme. Revise o nome e tente novamente")
     else:
-        print(f"Errro na solicitação: status {response.status_code}")
+        print(f"Erro na solicitação: status {response.status_code}")
+    return None
+
+def ExibeRespostas(resultado):
+    if resultado:
+        nome = resultado["title"]
+        descricao = resultado["overview"]
+        ano = resultado["release_date"].split("-")[0]
+
+        print(f"Nome do Filme: {nome}")
+        print(f"Ano: {ano}")
+        print(f"Descrição: {descricao}")
 
 if __name__ == "__main__":
     api_key = "ca2f7d89d43b8e1199ab5e7b51b55b99"
     nome_filme = input("Digite o nome do filme: ")
-    BuscaFilme(nome_filme, api_key)
+
+    resultado = BuscaFilme(nome_filme, api_key)
+
+    if resultado:
+        ExibeRespostas(resultado)
